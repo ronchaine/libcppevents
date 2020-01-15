ae229023ca07057a6add42d85a0f3e91fc0cd431
Jari Ronkainen <ronchaine@gmail.com>
2020-01-15 19:12:03 +0200

    Add GLFW integration stub

---
+++ include/cppevents/glfw.hpp
@@ -0,0 +1,26 @@
+#ifndef LIBCPPEVENTS_GLFW_HPP
+#define LIBCPPEVENTS_GLFW_HPP
+
+#include <epoxy/gl.h>
+#include <GLFW/glfw3.h>
+
+#include "input.hpp"
+#include "window.hpp"
+#include "events.hpp"
+
+#include <iostream>
+
+namespace cppevents
+{
+    native_source_type get_glfw_event_source(GLFWwindow* window);
+
+    /*
+    template <> error_code add_source<cppevents::input, SDL_Window*>(
+        SDL_Window* window,
+        event_queue& queue)
+    {
+    }
+    */
+}
+
+#endif
