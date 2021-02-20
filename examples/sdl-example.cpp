2b845f2f07590af139f5d5f3e698e3c99df513a6
Jari Ronkainen <ronchaine@gmail.com>
2021-02-20 10:25:24 +0200

    Litter the example with comments about what is happening

--- examples/sdl-example.cpp
+++ examples/sdl-example.cpp
@@ -4,6 +4,8 @@
 
 int main()
 {
+    // The first step is just basic SDL initialisation,
+    // as you would do it normally
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = nullptr;
@@ -24,17 +26,25 @@ int main()
     }
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
     SDL_RenderClear(renderer);
 
-    // adds SDL window as an event source
-    cppevents::add_source<cppevents::window_event>(window);
+    // Here we add SDL window as an event source, after this
+    // libcppevents can handle events coming from SDL,
 
-    cppevents::on_event<cppevents::keyboard_event>([&](cppevents::raw_event& raw){
+    // this is equivalent to just cppevents::add_source(window),
+    // but the template argument is shown for completeness
+    cppevents::add_source<cppevents::source::unspecified>(window);
+
+
+    //  Create a lambda function for handling keyboard events,
+    //  this might as well be a normal function
+    auto keyboard_handler = [&](cppevents::raw_event& raw) {
+        // Couldn't figure out a way to get rid of this cast,
+        // even though it's redundant, sorry.
         auto event = event_cast<cppevents::keyboard_event>(raw);
 
         std::cout << "Key " << static_cast<uint32_t>(event.scancode) << " ";
-        if (event.type == cppevents::keyboard_event::key_down)
+        if (event.action == cppevents::keyboard_event::key_down)
             std::cout << "pressed\n";
         else
             std::cout << "released\n";
@@ -44,18 +54,24 @@ int main()
             SDL_Quit();
             std::exit(0);
         }
-    });
+    };
 
+    // tell that when a keyboard event happens, we want it to be sent
+    // to the keyboard_handler function
+    cppevents::on_event<cppevents::keyboard_event>(keyboard_handler);
+
+    // using the lambda directly with mouse motion events
     cppevents::on_event<cppevents::mouse_motion>([&](cppevents::raw_event& raw){
         auto event = event_cast<cppevents::mouse_motion>(raw);
         std::cout << "Mouse moved by " << event.x_relative << "," << event.y_relative << " to " << event.x_pixels << "," << event.y_pixels << "\n";
     });
 
+    // ditto with buttons
     cppevents::on_event<cppevents::mouse_button>([&](cppevents::raw_event& raw){
         auto event = event_cast<cppevents::mouse_button>(raw);
 
         std::cout << "Mouse " << event.mouse_instance << " button " << static_cast<uint32_t>(event.button) << " ";
-        if (event.type == cppevents::mouse_button::button_down)
+        if (event.action == cppevents::mouse_button::button_down)
             std::cout << "pressed ";
         else
             std::cout << "released ";
@@ -67,6 +83,9 @@ int main()
     SDL_UpdateWindowSurface(window);
     while(true)
     {
+        // here we wait until we get an event
+        // and launch the appropriate handler when
+        // something happens
         cppevents::wait();
         SDL_RenderPresent(renderer);
     }
